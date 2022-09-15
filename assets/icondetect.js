83c734e86185afc8c4edd670e127f7c7a8313784
RblxLuauDevCode <113171451+RblxLuauDevCode@users.noreply.github.com>
2022-09-16 06:48:20 +0900

    v1.1.0 new features and bug fix on 2days

--- assets/icondetect.js
+++ assets/icondetect.js
@@ -1,9 +1,15 @@
+// please import jQuery and three.js first from codepen settings
+// or this will doexist not working and will handle into error.
+
+// iconDetect.js
 function getFavicon() {
-    var item = $('a.link_has_favicon');
-    item.each(function() {
-        var faviconUrl = $(this).attr('href');
-        $(this).prepend('<img src="http://www.google.com/s2/favicons?domain=' + faviconUrl + '">')
-    });
+	var item = $("a.loadIcon");
+	item.each(function () {
+		var faviconUrl = $(this).attr("href");
+		$(this).prepend(
+			'<img src="https://www.google.com/s2/favicons?domain=' + faviconUrl + '">'
+		);
+	});
 }
 
-getFavicon()
+getFavicon();
